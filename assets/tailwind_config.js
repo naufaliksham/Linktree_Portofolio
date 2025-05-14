03bf110596d043bf21a936aaaf44c1582de6628a
Naufal Iksham <naufal.iksham@gmail.com>
2025-05-14 10:46:25 +0700

    edit readme

---
+++ assets/tailwind_config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4F46E5",
+        darkGreen: {
+          950: "rgb(7 40 30)",
+          900: "#0b3d2e",
+          800: "#115e49",
+          700: "#198061",
+          600: "#22a177",
+        },
+      },
+      backdropBlur: {
+        xs: "2px",
+      },
+    },
+  },
+};
